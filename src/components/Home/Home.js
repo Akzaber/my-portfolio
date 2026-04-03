07f7dbac1f998eb9b2c6bf2ce8ae74fd6a91653c
AK Zaber <zaberkhan611@gmail.com>
2026-04-04 00:24:08 +0600

    modify bio

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -15,8 +15,7 @@ const Home = () => {
             Hello I am Zaber junior Front-end Developer
           </h1>
           <p className="text-lg">
-            My confidence possitive attitude and self-belief are my biggest
-            assets to take me a step closer to my success.
+            Hi, I am Abul Kashem Zaber, a passionate web developer. I build responsive and user-friendly web applications using HTML, CSS, JavaScript, and modern tools. I love turning ideas into interactive websites and continuously learning new technologies.
           </p>
           <div>
             <Link
